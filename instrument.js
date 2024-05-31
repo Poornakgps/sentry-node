ba432401aca0a5ddfb05e9ddc2d4f7e1d9be9ebf
Poornachandra123 <96694008+Poornachandra123@users.noreply.github.com>
2024-05-31 11:56:17 +0530

    Initial commit

---
+++ instrument.js
@@ -0,0 +1,15 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: "https://4ad9237083974e401beffdb9df20bda6@o4507338110140416.ingest.us.sentry.io/4507349396619264",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
